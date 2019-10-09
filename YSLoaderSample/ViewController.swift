f79096791e5c480f1ac6f45f65a2dc65fc3197a3
Yahya Saddiq <yahya.saddiq1@vodafone.com>
2019-10-09 07:52:24 +0200

    add scrollable tableView

--- YSLoaderSample/ViewController.swift
+++ YSLoaderSample/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import YSLoader
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let imageURL = "https://limemicro.com/app/uploads/2018/08/Vodafone_1024x768_HeaderSafe.jpg"
+        let loader: YSLoader = YSLoader()
+        loader.image(with: imageURL) { (image) in
+            guard let image = image else {
+                return
+            }
+            DispatchQueue.main.async {
+                self.imageView.image = image
+            }
+        }
     }
 
 
