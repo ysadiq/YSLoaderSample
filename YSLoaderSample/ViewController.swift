e9ef1add4b5830168bb60ef8665b0529c460814a
Yahya Saddiq <yahya.saddiq1@vodafone.com>
2019-10-09 03:39:22 +0200

    Initial Commit

---
+++ YSLoaderSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YSLoaderSample
+//
+//  Created by Yahya Saddiq on 10/9/19.
+//  Copyright © 2019 Yahya Saddiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
